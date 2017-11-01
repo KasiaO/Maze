07b1683afec5e69a8ee54bbf77f0939e40bf199b
tomek <katarzyna.oktaba@wp.pl>
2017-11-01 15:00:05 +0100

    Start

---
+++ MazeGame/src/com/game/Maze.java
@@ -0,0 +1,17 @@
+package com.game;
+
+import com.game.maze.Room;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by tomek on 2017-11-01.
+ */
+public class Maze {
+
+    public static void main(String[] args) {
+
+
+    }
+}
---
+++ MazeGame/src/com/game/maze/Room.java
@@ -0,0 +1,10 @@
+package com.game.maze;
+
+/**
+ * Created by tomek on 2017-11-01.
+ */
+public interface Room {
+
+    void draw();
+
+}
